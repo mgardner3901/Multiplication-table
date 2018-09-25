10f4d448184ecc4d5de14db3037403427b11d927
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-09-25 12:19:26 -0400

    Updated from Brython Server: 9/25/2018 12:19:25 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Morgan Gardner
+Credit: None
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,7 +21,6 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
-
 
 width=int(input("Width of multiplication table: "))
 height=int(input("Height of multiplication table: "))
